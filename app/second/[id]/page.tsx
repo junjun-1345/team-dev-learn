225cf0356d7a55ec419fb2d4cefce096654af1bb
seikin <haru.mo.1113@gmail.com>
2024-12-12 18:48:21 +0900

    ページ遷移ができるようになった

--- app/second/[id]/page.tsx
+++ app/second/[id]/page.tsx
@@ -1,25 +1,63 @@
 // app/github-search/page.tsx
 "use client";
 
-import { useRouter } from "next/router";
-import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import React, { useState,useEffect } from "react";
 import Image from "next/image";
 import { GitHubRepo, GitHubUser } from "./github2";
+import {ParsedUrlQuery} from "querystring";
+import { parseAppSegmentConfig } from "next/dist/build/segment-config/app/app-segment-config";
 
-export default function SecondDetail() {
+export default function SecondDetail({ params }: { params: { id: string } }) {
+ {/* interface QueryParams extends
+  ParsedUrlQuery{ id : string}
+
+  
   const router = useRouter();
+  const {id}=router.query as QueryParams;*/}
 
   const [query, setQuery] = useState<string>("");
   const [results, setResults] = useState<GitHubRepo[]>([]);
   const [selectedUser, setSelectedUser] = useState<GitHubUser | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
+  
+  
+
+  const fetchUserDetails = async (username: string) => {
+    try {
+      const response = await fetch(`https://api.github.com/users/${username}`, {
+        headers: {
+          Accept: "application/vnd.github.v3+json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`GitHub API error: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setSelectedUser(data);
+    } catch (err) {
+      setError(
+        err instanceof Error
+          ? err.message
+          : "ユーザー情報の取得中にエラーが発生しました。"
+      );
+    }
+  };
+{/*if(router.query.id===true){fetchUserDetails(router.query.login)}  ここがusernameならいいのか*/}
+  
+  {/*fetchUserDetails(router.query.login);*/}
+  useEffect(()=>{
+    
+  },[])
 
   return (
+    
     <div>
-      {/*<h1>{router.query.name}</h1>
-        <p>{`熟練度: ${router.query.proficiency}`}</p>*/}
-
+      
+      hello
       {selectedUser && (
         <aside className="sticky top-4">
           <div className="border rounded-lg p-4 bg-white">
